8075af459253eb786b485bd7b847ffc6d839cae4
denev <65989857+deneb0723@users.noreply.github.com>
2021-05-03 13:03:22 +0900

    Morgan 미들웨어 설치 및 적용

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => res.send("Hello");
+
+app.use(logger);
+app.get("/", home);
 
 const handleListen = () =>
   console.log(`✅ localhost:${PORT} 서버에 연결되었습니다. ✈️`);
