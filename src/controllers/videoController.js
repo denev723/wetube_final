c3181988ceab084064e92b9814127d73f5516d07
denev <65989857+deneb0723@users.noreply.github.com>
2021-05-07 08:11:00 +0900

    templete 일부 생성(home, watch video, edit video)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,41 @@
-export const home = (req, res) => res.send("Home");
-export const watchVideo = (req, res) => res.send("Watch Video");
-export const editVideo = (req, res) => res.send("Edit Video");
+const fakeUser = {
+  username: "Denev",
+  loggedIn: true,
+};
+
+export const home = (req, res) => {
+  const videos = [
+    {
+      title: "video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
+export const watchVideo = (req, res) =>
+  res.render("watchVideo", { pageTitle: `Watch Videos` });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const uploadVideo = (req, res) => res.send("Upload Video");
 export const search = (req, rs) => res.send("Search");
